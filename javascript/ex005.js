54c0ae53c2b2c006670b5fa4993c3cff895351dc
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-04-19 19:42:28 -0300

    jj

---
+++ javascript/ex005.js
@@ -0,0 +1,12 @@
+let vel = 60
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if(vel > 60.5)
+{
+    console.log(`Voçê ultrapassou a velocidade permitida MULTADO!!!`)
+}
+else
+{
+    console.log(`Velocidade adequada!!`)
+}

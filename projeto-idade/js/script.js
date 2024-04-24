2c30c99999fa02d79974c5a2b104f2b2e4528a5c
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-04-23 22:18:19 -0300

    atualizei

--- projeto-idade/js/script.js
+++ projeto-idade/js/script.js
@@ -6,18 +6,21 @@ function clicar()
     let div = document.getElementById("container")
 
     let genero = document.getElementsByName("sexo")
-    if(Number(ano.value) == 0 || ano.value >= agora)
+    let img = document.getElementById("foto")
+    if(ano.value == 0 || ano.value >= agora || ano.value < 1930)
     {   
         alert("Erro!! Verifique os dados e tente novamente")
 
     }
-    else if(genero[0].checked)
+    else if(genero[0].checked && idade >= 50)
     {
-        div.innerHTML = `Identificamos um Homem de ${idade} anos.`
+        div.innerHTML = `Identificamos um Homem idoso de ${idade} anos.`
+        img.src = `img/man-old.png`
     }
-    else if(genero[1].checked)
+    else if(genero[1].checked && idade >= 50)
     {
-        div.innerHTML = `Identificamos uma Mulher de ${idade} anos.`
+        div.innerHTML = `Identificamos uma Mulher idosa de ${idade} anos.`
+        img.src = `img/woman-old.png`
     }
 
 }

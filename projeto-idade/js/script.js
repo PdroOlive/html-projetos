9a2ab081bec7f4d6e3d85544a6b352c22384de58
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-04-25 21:15:49 -0300

    iu

--- projeto-idade/js/script.js
+++ projeto-idade/js/script.js
@@ -14,13 +14,55 @@ function clicar()
     }
     else if(genero[0].checked && idade >= 50)
     {
-        div.innerHTML = `Identificamos um Homem idoso de ${idade} anos.`
-        img.src = `img/man-old.png`
+        div.innerHTML = `Identificamos um Homem idoso com ${idade} anos.`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/man-old.png")
     }
     else if(genero[1].checked && idade >= 50)
     {
-        div.innerHTML = `Identificamos uma Mulher idosa de ${idade} anos.`
-        img.src = `img/woman-old.png`
+        div.innerHTML = `Identificamos uma Mulher idosa com ${idade} anos.`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/woman-old.png")
     }
+    else if(genero[0].checked && idade < 50 && idade >= 26)
+    {
+        div.innerHTML = `Identificamos um Homem com ${idade} anos`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/man.png")
+    }
+    else if(genero[1].checked && idade < 50 && idade >= 26)
+    {
+        div.innerHTML = `Identificamos uma Mulher com ${idade} anos`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/woman.png")
+    }
+    else if(genero[0].checked && idade > 10 && idade < 26)
+    {
+        div.innerHTML = `Identificamos um Jovem com ${idade} anos`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/man-young.png")
+
+    }
+    else if(genero[1].checked && idade > 10 && idade < 26 )
+    {
+        div.innerHTML = `Identificamos uma Jovem com ${idade} anos`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/woman-young.png")
+    }
+    else if(genero[0].checked && idade >= 1 && idade <= 10)
+    {
+        div.innerHTML = `Identificamos uma Criança do sexo masculino com ${idade} anos`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/kid-boy.png")
+    }
+
+    else if(genero[1].checked && idade >= 1 && idade <= 10)
+    {
+        div.innerHTML = `Identificamos uma Criança do sexo feminino com ${idade} anos`
+        document.querySelector("main").style.height = "600px"
+        img.setAttribute("src", "img/kid-girl.png")
+    }
+
+    div.appendChild(img)
 
 }

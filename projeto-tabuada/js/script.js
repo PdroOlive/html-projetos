93ef51eeb2f13189d218c9dde42a43cbe326fa8d
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-04-29 21:23:01 -0300

    y

--- projeto-tabuada/js/script.js
+++ projeto-tabuada/js/script.js
@@ -4,12 +4,23 @@ function clicou()
     let res = document.getElementById("resul")
     let cont = 1
     let tabuada = 0
-    let n = Number(num.value)
     res.innerHTML = ""
-    while(cont <= 10)
+    if(num.value.length == "")
     {
-        tabuada = n * cont;
-        res.innerHTML += `${n} x ${cont} = ${tabuada} <br>`
-        cont++
+        res.innerHTML = `ERRO!!! Entre com um valor válido!`
+        res.style.color = "red"
+    }
+    else
+    {
+        let n = Number(num.value)
+        res.style.color = "black"
+        res.innerHTML = `TABUADA DO ${n} <br>`
+        res.style.border = "1px solid black"
+        while(cont <= 10)
+        {
+            tabuada = n * cont;
+            res.innerHTML += `${n} x ${cont} = ${tabuada} <br>`
+            cont++
+        }
     }
 }

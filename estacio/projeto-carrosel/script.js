f888103b333aed3995eff5264bc05ea2a6fd6e2f
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-05-13 21:17:17 -0300

    atualizei o projeto academia

--- estacio/projeto-carrosel/script.js
+++ estacio/projeto-carrosel/script.js
@@ -2,6 +2,8 @@ let carouselImages = document.querySelector('.carousel-images');
 let images = document.querySelectorAll('.carousel-images img');
 let currentIndex = 0;
 let totalImages = images.length;
+let imageYearlyOn = document.getElementById("img-anual");
+let imageYearlyOff = document.getElementById("img-anual");
 
 function nextImage() {
   currentIndex = (currentIndex + 1) % totalImages;
@@ -13,4 +15,21 @@ function updateCarousel() {
   carouselImages.style.transform = `translateX(${newPosition})`;
 }
 
+
+function mouseOnImage()
+{
+  imageYearlyOn.innerText = `Oi`;
+
+}
+
+function mouseOffImage()
+{
+  
+}
+
+
+
+
+imageYearlyOn.addEventListener("mouseenter", mouseOnImage);
+imageYearlyOff.addEventListener("mouseout", mouseOffImage);
 setInterval(nextImage, 3000); // Troca de imagem a cada 3 segundos
